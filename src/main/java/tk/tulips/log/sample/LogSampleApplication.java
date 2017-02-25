526bf929d7a964222a574bc3b04c1e2905f8ff8d
applejuice <applejuice@efficentsoft.com>
2017-02-25 23:13:11 +0900

    first init

---
+++ src/main/java/tk/tulips/log/sample/LogSampleApplication.java
@@ -0,0 +1,12 @@
+package tk.tulips.log.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogSampleApplication.class, args);
+	}
+}
